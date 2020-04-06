27fd7321c514fa93c7af64eab6d867163ff94c82
Maciej Majchrzak <maciek.majchrzak15@gmail.com>
2020-04-06 18:57:14 +0200

    Addign account controller

---
+++ src/BuildingCondition/BuildingCondition.Mvc/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingCondition.Mvc.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
