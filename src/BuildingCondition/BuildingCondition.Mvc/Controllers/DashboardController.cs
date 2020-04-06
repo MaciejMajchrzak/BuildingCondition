62ef84e091722153f82eecf20a16e727624a8349
Maciej Majchrzak <maciek.majchrzak15@gmail.com>
2020-04-06 19:02:51 +0200

    Adding dashboard controller

---
+++ src/BuildingCondition/BuildingCondition.Mvc/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingCondition.Mvc.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
